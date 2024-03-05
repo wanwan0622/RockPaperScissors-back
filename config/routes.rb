3d8671ef8625f9cac9fdc802f3afb804de3f3afb
Ayari Takezawa <minnie20000622@yahoo.co.jp>
2024-03-05 23:53:45 +0900

    routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  get "up" => "rails/health#show", as: :rails_health_check
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
+    end
+  end
 end
