06cab02c1e7cf39e47b06fd3d3ebe98915085ee1
Ayari Takezawa <minnie20000622@yahoo.co.jp>
2024-03-18 20:43:02 +0900

    価値観の相手テーブルを作成

---
+++ db/migrate/20240318113254_create_partners.rb
@@ -0,0 +1,11 @@
+class CreatePartners < ActiveRecord::Migration[6.1]
+  def change
+    create_table :partners do |t|
+      t.string :name, null: false, limit: 10
+
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
