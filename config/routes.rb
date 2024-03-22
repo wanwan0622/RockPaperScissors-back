2db89180b25b94a101a3e8d52c319b2210990492
Ayari Takezawa <minnie20000622@yahoo.co.jp>
2024-03-23 05:11:33 +0900

    recousesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   # 議事録パート
   resources :reports, except: %i[show destroy]
   resources :report_details, except: %i[show destroy]
+  resources :to_dos, except: %i[show destroy]
 end
